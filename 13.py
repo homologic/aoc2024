93491a2661dfe3ed73df6446f03c9c402f9ad1df
Antonia Pérez-Cerezo <antonia@antonia.is>
2024-12-13 14:10:09 +0100

    Day 13

---
+++ 13.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from sage.all import *
+import sys
+from re import findall
+
+lines = [] 
+
+cur = []
+
+s = 0
+
+def solve(cur) :
+    sol = matrix(ZZ,2,2,cur[:4]).transpose().solve_right(vector(ZZ,cur[4:]))
+    print(sol)
+    is_valid = True
+    for k in sol :
+        if k not in ZZ or k < 0 :
+            is_valid = False
+            break
+    if is_valid:
+        return sol[0] * 3 + sol[1]
+    return 0
+
+for line in sys.stdin :
+    if line == "\n" :
+        s += solve(cur)
+        cur = []
+        continue
+    for i in map(int,findall(r"[0-9]+", line)) :
+        cur.append(i)
+
+s+= solve(cur)
+print(s)
